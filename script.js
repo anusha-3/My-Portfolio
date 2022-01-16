6296d90cbb5f848d8f5c885d47eee7d3dd548271
anusha-3 <anushabera009@gmail.com>
2022-01-16 22:18:59 +0530

    style of project page

--- script.js
+++ script.js
@@ -1,50 +1,20 @@
-$(document).ready(function () {
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
 
-    $('#menu').click(function () {
-      $(this).toggleClass('fa-times');
-      $('.navbar').toggleClass('nav-toggle');
-    });
-  
-    $(window).on('scroll load', function () {
-  
-      $('#menu').removeClass('fa-times');
-      $('.navbar').removeClass('nav-toggle');
-  
-      if ($(window).scrollTop() > 0) {
-        $('.scroll-top').show();
-      } else {
-        $('.scroll-top').hide();
-      }
-  
-      $('section').each(function () {
-  
-        let height = $(this).height();
-        let offset = $(this).offset().top - 200;
-        let id = $(this).attr('id');
-        let top = $(window).scrollTop();
-  
-        if (top > offset && top < offset + height) {
-          $('.navbar ul li a').removeClass('active')
-          $('.navbar').find(`[href="#${id}"]`).addClass('active');
-        }
-  
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
       });
-  
-    });
-  
-    $('a[href*="#"]').on('click', function (e) {
-  
-      e.preventDefault();
-  
-      $('html, body').animate({
-  
-        scrollTop: $($(this).attr('href')).offset().top,
-  
-      },
-        500,
-        'linear'
-      )
-  
-    })
-  
+    }
   });
+};
